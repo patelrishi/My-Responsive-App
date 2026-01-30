1a077a2436a65f0fe503c208ddfeb360f8b8afa4
patelrishi <patelsai9696@gmail.com>
2026-01-30 17:13:31 +0530

    add valid postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+  },
 };
-
-export default config;
-=======
-// const config = {
-//   plugins: ["@tailwindcss/postcss"],
-// };
-
-// export default config;
->>>>>>> 458b0d971b7d55407206a2166155dd39566181b0
